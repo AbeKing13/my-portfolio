8d75d87b8ed3d0730d0eba3b6a4a33495029fb32
Amir Hammar <amir.hammar.1@ens.etsmtl.ca>
2025-02-08 15:41:56 -0500

    Issue #1: Added projects with images and short descriptions

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -12,7 +12,7 @@ function Project() {
               src={require("../assets/images/projects/digiclipse.png")}
               className="zoom"
               alt=""
-              width="70%"
+              width="80%"
               style={{ display: "flex", justifyContent: "center" }}
             />
           </a>
@@ -51,6 +51,74 @@ function Project() {
         <div className="project">
           <a>
             <img
+              src={require("../assets/images/projects/aspire.png")}
+              className="zoom"
+              alt="thumbnail"
+              width="100%"
+              style={{ display: "flex", justifyContent: "center" }}
+            />
+          </a>
+          <a>
+            <h2>Aspire</h2>
+          </a>
+            <p>
+            Develop a <b>voice assistant</b> named <b>Aspire</b> (still under
+            development) for <b>computer control</b>. It enables users to{" "}
+            <b>open applications</b>, <b>navigate the web</b>, and ask questions
+            to <b>ChatGPT</b>. The project leverages <b>OpenAI GPT-3</b> and <b>Python</b>,
+            utilizes <b>APIs</b> for integration, and incorporates{" "}
+            <b>audio file management</b>.
+            </p>
+        </div>
+        <div className="project">
+          <a>
+            <img
+              src={require("../assets/images/projects/voice_translator.png")}
+              className="zoom"
+              alt="thumbnail"
+              width="100%"
+              style={{ display: "flex", justifyContent: "center" }}
+            />
+          </a>
+          <a>
+            <h2>Voice Translator</h2>
+          </a>
+          <p>
+            <p>
+              Developed a <b>Python-powered</b> voice translator that converts{" "}
+              <b>speech to text</b> using <b>OpenAI Whisper</b> and <b>NumPy</b>{" "}
+              for processing, translates the extracted text into Japanese via an{" "}
+              <b>external API</b>, and synthesizes the translated text back into
+              speech using advanced speech processing techniques.
+            </p>
+          </p>
+        </div>
+        <div className="project">
+          <a>
+            <img
+              src={require("../assets/images/projects/database.png")}
+              className="zoom"
+              alt="thumbnail"
+              width="100%"
+              style={{ display: "flex", justifyContent: "center" }}
+            />
+          </a>
+          <a>
+            <h2>Web Database Interface</h2>
+          </a>
+          <p>
+            <p>
+              Set up a <b>web database interface</b> on a <b>Raspberry Pi</b> server with a{" "}
+              <b>web interface</b> that simplifies <b>displaying</b>, <b>entering</b>, and{" "}
+              <b>manipulating data</b>. Leveraging <b>web development</b> best practices, the project 
+              integrates <b>SQL</b> capabilities with a user-friendly <b>user interface</b> and utilizes{" "}
+              <b>MariaDB</b> for robust database management.
+            </p>
+          </p>
+        </div>
+        <div className="project">
+          <a>
+            <img
               src={require("../assets/images/projects/website.png")}
               className="zoom"
               alt="thumbnail"
@@ -66,30 +134,6 @@ function Project() {
             and experiences. Implemented <b>React</b>, <b>HTML</b>, <b>CSS</b>,
             and <b>JavaScript</b> to create a responsive and interactive
             website.
-          </p>
-        </div>
-        <div className="project">
-          <a>
-            <img
-              src={require("../assets/images/projects/voice_translator.png")}
-              className="zoom"
-              alt="thumbnail"
-              width="70%"
-              style={{ display: "flex", justifyContent: "center" }}
-            />
-          </a>
-          <a>
-            <h2>Voice Translator</h2>
-          </a>
-          <p>
-            <p>
-              Developed a <b>Python-powered</b> voice translator that
-              converts <b>speech to text</b> using <b>OpenAI Whisper</b> and{" "}
-              <b>NumPy</b> for processing, translates the extracted text into{" "}
-              Japanese via an <b>external API</b>, and synthesizes the
-              translated text back into speech using{" "}
-              advanced speech processing techniques.
-            </p>
           </p>
         </div>
       </div>
